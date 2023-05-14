7fd7179a6be4139441778f22b5fb5a656196a2ce
Karam Gbaren <karam99.k@hotmail.com>
2023-05-14 21:06:29 +0300

    Before using useReducer hook

--- CustomNavigations_RoutingSystems_TailwindCSS/app/src/pages/CounterPage.js
+++ CustomNavigations_RoutingSystems_TailwindCSS/app/src/pages/CounterPage.js
@@ -1,17 +1,43 @@
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
-import useCounter from "../hooks/useCounter"; 
+import Panel from "../components/Panel"
+// import useCounter from "../hooks/useCounter"; 
 
 function CounterPage({ initialCount }) {
-    const {count, increment} = useCounter({initialCount});
+    // const {count, increment} = useCounter({initialCount});
+    const [count, setCount] = useState(initialCount);
+    const [valueToAdd, setValueToAdd] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        setCount(count - 1);
+    }
+    const handleChange = (e) => {
+        setValueToAdd(parseInt(e.target.value) || 0);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setCount(count + valueToAdd);
+        setValueToAdd(0);
+    }
 
     return (
-        <div>
-            <h1>
+        <Panel className="m-3">
+            <h1 className="text-lg">
                 Count is {count}
             </h1>
-            <Button onClick={increment}>Increment</Button>
-        </div>
+            <div className="flex flex-row">
+                <Button onClick={increment}>Increment</Button>
+                <Button onClick={decrement}>Decrement</Button>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input type="number" className="p-1 m-3 bg-gray-50 border border-gray-300" value={valueToAdd || ""} onChange={handleChange}></input>
+                <Button>Add it!</Button>
+            </form>
+        </Panel>
     )
 }
 
